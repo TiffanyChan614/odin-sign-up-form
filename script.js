6d62ff782511c89af473bf35d32433aedff7ff7c
TiffChan <tiffanychan1999614@gmail.com>
2022-10-24 14:55:53 -0400

    Refactor: create .error class to handle unmatched passwords styling

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ function verifyPassword() {
     const password = document.querySelector("#pwd");
     const confirmPassword = document.querySelector("#confirm-pwd");
     if (password.value !== confirmPassword.value) {
-        password.style.border = "1.5px solid #ff5353";
-        confirmPassword.style.border = "1.5px solid #ff5353";
+        password.className = "error";
+        confirmPassword.className = "error";
     }
     const pwdErrorMsg = document.querySelector("#pwd-error-msg");
     pwdErrorMsg.style.display = "block";
