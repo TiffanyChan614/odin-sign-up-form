72921816efabf28face22c44fd04c182ae48b2da
TiffChan <tiffanychan1999614@gmail.com>
2022-11-03 20:04:38 -0400

    Fix: Password error message only appears when passwords mismatch

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 function verifyPassword() {
     const password = document.querySelector("#pwd");
     const confirmPassword = document.querySelector("#confirm-pwd");
+    const pwdErrorMsg = document.querySelector(".pwd-error-msg");
     if (password.value !== confirmPassword.value) {
         password.className = "error";
         confirmPassword.className = "error";
+        pwdErrorMsg.style.display = "block";
     }
-    const pwdErrorMsg = document.querySelector(".pwd-error-msg");
-    pwdErrorMsg.style.display = "block";
     return password.value === confirmPassword.value;
 }
